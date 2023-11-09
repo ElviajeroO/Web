1ac298a0eef3ba665abc23b69c2063546e656603
João Henrique Ferreira Krüger <130417561+ElviajeroO@users.noreply.github.com>
2023-11-08 22:41:58 -0300

    Update card.js

--- template/card/js/card.js
+++ template/card/js/card.js
@@ -1,30 +1,33 @@
-	window.onload = async function(){
+window.onload = async function(){
 	var promise = await fetch("../php/select.php", {
 		method: 'GET',
 	});
 	 
 	var dados = await promise.json();
-
+	
 	console.log(dados);
 
 	for(var i = 0; i < dados.length; i++){
-		var conteudo = 
+		var card = 
 		`<div class='card'>
-				<div class='card-nome'>
-					<a>${dados[i].nome}</a>
-				</div>
-				<div class='card-imagem'>
-					<img src='../upload/${dados[i].nome}'></img>
-				</div>
-				<div class='card-valor'>
-					<a>R$${dados[i].preco}</a>
-				</div>
-				<div class='card-acao' onclick='Apagar(${dados[i].id_produto})'>
-					<a>Apagar produto</a>
-				</div>
+			<div class='card-nome'>
+				<a>${dados[i].nome}</a>
+			</div>
+			<div class='card-imagem'>
+				<img src='../upload/${dados[i].nome}'></img>
+			</div>
+			<div class='card-adic'>
+				<div class='card-cor'><a>${dados[i].cor}</a></div>
+				<div class='card-tam'><a>${dados[i].tamanho}</a></div>
+			</div>
+			<div class='card-valor'>
+				<a>R$${dados[i].preco}</a>
+			</div>
+			<div class='card-acao' onclick='Apagar(${dados[i].id_produto})'>
+				<a>Apagar produto</a>
+			</div>
 		</div>`
 		
-		document.getElementById('produtos').innerHTML += conteudo;
+		document.getElementById('produtos').innerHTML += card;
 	}
 }
-
