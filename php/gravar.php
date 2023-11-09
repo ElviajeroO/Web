8241835cd52869736ec715d18820ded5b53cf346
João Henrique Ferreira Krüger <130417561+ElviajeroO@users.noreply.github.com>
2023-11-08 23:03:45 -0300

    Add files via upload

--- php/gravar.php
+++ php/gravar.php
@@ -1,15 +1,17 @@
 <?php 
 
-	$nome = $_POST['none'];
+	$nome = $_POST['nome'];
 	$preco = $_POST['preco'];
+	$cor = $_POST['cor'];
+	$tamanho = $_POST['tamanho'];
 	$file = $_FILES['arquivo'];
 
 
 	$connection = mysqli_connect('127.0.0.1:3306', 'root', 'root','web');
-	if (empty($nome) or empty($preco) or empty($file['type']))
+	if (empty($nome)||empty($preco)||empty($cor)||empty($tamanho)||empty($file['type']))
 		echo 'Preencha todos os campos';
 	else
-		$query = "INSERT INTO produtos (nome, preco) VALUES('$nome', '$preco')";
+		$query = "INSERT INTO produtos (nome, preco, cor, tamanho) VALUES('$nome', '$preco', '$cor', '$tamanho')";
 		mysqli_query($connection, $query);
 		$novo = "../upload/".$nome;
                 move_uploaded_file($file["tmp_name"], $novo);
