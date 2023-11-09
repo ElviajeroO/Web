773edfee0133f7f0e6c1f6d5516f43b67f155648
João Henrique Ferreira Krüger <130417561+ElviajeroO@users.noreply.github.com>
2023-11-08 23:04:26 -0300

    Add files via upload

--- js/gravar.js
+++ js/gravar.js
@@ -1,14 +1,14 @@
 async function Gravar(){
 
-	var form = document.getElementById('cadastra-forms');
-	var file = document.getElementById('file').files;
-	var dados = new FormData(form);
-	dados.append('arquivo', file[0]);
-	console.log(dados);
-	var promise = await fetch('../php/gravar.php', {
-		method: 'POST',
-		body: dados
-	});
+		var form = document.getElementById('cadastra-forms');
+		var file = document.getElementById('file').files;
+		var dados = new FormData(form);
+		dados.append('arquivo', file[0]);
+		console.log(dados);
+		var promise = await fetch('../php/gravar.php', {
+			method: 'POST',
+			body: dados
+		});
 	
 	var resposta = await promise.text();
 
@@ -19,5 +19,13 @@ async function Gravar(){
 
 }
 
+function menuShow(){
 
-				
+	let menuMobile = document.querySelector('.header-mobile-menu');
+
+	if (menuMobile.classList.contains('open')){
+		menuMobile.classList.remove('open');
+	} else{
+		menuMobile.classList.add('open');
+	}
+}
